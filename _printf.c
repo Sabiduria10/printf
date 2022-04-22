6791a3c5b727837fb59ddc096db5742846fa213c
francis109 <francismwape0@gmail.com>
2022-04-22 11:52:27 -0700

    _printf.c commit

--- _printf.c
+++ _printf.c
@@ -1,49 +1,48 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
+ * _printf - Build out the printf function
+ * @format: string passed with possible format specifiers
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	int i, blen, hlen;
+	double totalBuffer;
+	double *total;
+	va_list argp;
+	char buffer[BUFSIZE], *holder;
+	char *(*pointer_get_valid)(va_list);
 
-	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+	for (i = 0; i < BUFSIZE; i++)
 	{
-		init_params(&params, ap);
-		if (*p != '%')
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
+		buffer[i] = 0;
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	totalBuffer = 0;
+	pointer_get_valid = NULL;
+	total = &totalBuffer;
+	va_start(argp, format);
+	for (i = blen = hlen = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			pointer_get_valid = get_valid_type(format[i + 1]);
+			holder = (pointer_get_valid == NULL) ?
+				found_nothing(format[i + 1]) :
+				pointer_get_valid(argp);
+			hlen = _strlen(holder);
+			blen = alloc_buffer(holder, hlen, buffer, blen, total);
+			i++;
+		}
+		else
+		{
+			holder = ctos(format[i]);
+			blen = alloc_buffer(holder, 1, buffer, blen, total);
+		}
+	}
+	va_end(argp);
+	_puts(buffer, blen);
+	return (totalBuffer + blen);
 }
